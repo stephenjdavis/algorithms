5126f337329cd7b28118fe474169a617047782f0
Stephen Davis <sdavis.usa@gmail.com>
2019-09-03 09:54:29 -0700

    Updated comments.

--- maxprofit.py
+++ maxprofit.py
@@ -2,11 +2,16 @@ def get_max_profit(A):
     data = []
     max_profit = 0
     n = len(A)
+    # The day's price (will end up being our day to buy)
     i = 0
+    # The day's prices we might compare info with... (will end up being our day to sell)
     j = 0
     for i in range(n):
         for j in range(n):
+            #... but only if they come after each day we're comparing.
             if (j > i): 
+                # ... we set our max profit and update it when the difference between 
+                # sell and buy is the greatest
                 if ((A[j] - A[i] > max_profit)):
                     max_profit = A[j] - A[i]
                     buy_date = i + 1
