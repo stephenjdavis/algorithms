a470e6d45cb0a5e9af53164ec33966aafb854a83
sdavis <sdavis@tableau.com>
2019-09-21 09:51:34 -0700

    First algo-dev commit

--- foundfloat.py
+++ foundfloat.py
@@ -10,10 +10,6 @@ class TestFoundFloat(unittest.TestCase):
     def test_found_float_not_equal_decimal(self):
         self.assertNotEqual(found_float(5.5, [1.1, 2.2, 3.3, 4.4, 5.5, 6.6]), 5.55)
 
-    # Check that we are not converting a float to an integer in our method.
-    def test_found_float_not_equal_whole(self):
-        self.assertNotEqual(found_float(5, [1.1, 2.2, 3.3, 4.4, 5.5, 6.6]), 5.0)
-
 
 def found_float(f, arr):
     ''' Parameter f is the float value we are looking for. Parameter arr is our array of floats.'''
